480e30fded4216015f0fad9c6c1c2de7d064f1ff
Bruno Marra de Melo <brunomarrademelo@MacBook-Pro-de-Bruno.local>
2019-06-22 12:23:58 -0300

    Starta e cria clientes e servidores

---
+++ Jogador2/src/jogador2/Jogador2.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jogador2;
+
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.util.Date;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author brunomarrademelo
+ */
+public class Jogador2 {
+
+    static final int PORT = 50000;
+    
+    public static void main(String[] args) {
+        try {
+          Socket cliente = new Socket("localhost", PORT);
+          ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
+          Date data_atual = (Date)entrada.readObject();
+          JOptionPane.showMessageDialog(null,"Data recebida do servidor:" + data_atual.toString());
+          entrada.close();
+          System.out.println("Conexão encerrada");
+        }
+        catch(Exception e) {
+          System.out.println("Erro: " + e);
+        }
+    }
+}
